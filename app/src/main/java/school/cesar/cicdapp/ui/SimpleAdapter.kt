4455cd5a07e36db4379ef343e1a87501ef0fa759
Eduardo Maia <ehammo@motorola.com>
2021-10-18 20:21:33 -0300

    Initial commit

---
+++ app/src/main/java/school/cesar/cicdapp/ui/SimpleAdapter.kt
@@ -0,0 +1,33 @@
+package school.cesar.cicdapp.ui
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import school.cesar.cicdapp.R
+import school.cesar.cicdapp.databinding.ListItemBinding
+
+class SimpleAdapter(private val list: List<String>) : RecyclerView.Adapter<SimpleAdapter.VH>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        val binding = ListItemBinding.bind(view)
+        return VH(binding)
+    }
+
+    override fun getItemCount():Int {
+        Log.i("duds", "getItemCount ${list.size}")
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        holder.title.text = list[position]
+    }
+
+    class VH(itemView: ListItemBinding): RecyclerView.ViewHolder(itemView.root) {
+        val title: TextView = itemView.title
+    }
+}
